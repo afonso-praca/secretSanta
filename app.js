cceaa64476e9371f73f5d2818be11d0923a79ebd
afonso <afonsoinfo@gmail.com>
2015-12-21 10:35:49 -0200

    uses email js to send emails

--- app.js
+++ app.js
@@ -1,45 +1,55 @@
-var request = require('request');
 var _ = require('underscore');
+var emailjs = require('emailjs');
 
-function pessoa(nome, email){
-	this.nome = nome,
-	this.email = email
+var config = {
+  "user": "",
+  "password": "",
+  "host": "",
+  "ssl": true
 };
 
-function shuffle(o){
-    for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
-    return o;
-};
+var server = emailjs.server.connect(config);
+
+function Pessoa(nome, email){
+	this.nome = nome;
+	this.email = email;
+}
 
 function sendEmail(pessoa, amigo){
-	request.post(
-	    'http://afonsopraca.com.br/aoc/envia_ao_amigo.php',
-	    { form:
-	    	{
-	    		"seu_nome": pessoa.nome,
-	    		"seu_email": pessoa.email,
-	    		"nome_amigo": amigo.nome,
-	    		"email_amigo": amigo.email
-	    	}
-	    },
-	    function (error, response, body) {
-	        if (!error && response.statusCode == 200) {
-	            console.log("enviou");
-	        }
-	    }
-	);
+  var text = "Olá " + pessoa.nome + "!<br>";
+  text += "Seu amigo oculto é " + amigo.nome + ".<br>";
+  text += "Que o natal seja repleto de alegria.<br><br>";
+  text += "Um Beijo do Papai Noel, da Dani e do Afonso";
+
+  options = {};
+  options.to = pessoa.nome + " <" + pessoa.email + ">";
+  options.from = "Papai Noel do Humaitá <loja@pilateslovers.com.br>";
+  options.subject = "Amigo Oculto Soria Texeira";
+  options.text = text;
+  options.attachment =
+    [
+      {
+        data: text,
+        alternative: true
+      }
+    ];
+
+  server.send(options, function(){
+    console.log("email enviado");
+  });
 }
 
 var arrayPessoas = [
-	new pessoa("Afonso Praça", "afonsoinfo@gmail.com"),
-	new pessoa("Daniela Soria", "danisoriafisio@gmail.com")
+	new Pessoa("Nome", "email@email.com"),
+	new Pessoa("Nome2", "email2@email.com")
 ];
 
-arrayPessoas = shuffle(arrayPessoas);
+arrayPessoas = _.shuffle(arrayPessoas);
 
-for (var i = 0; i < arrayPessoas.length; i++) {
-	if (i != arrayPessoas.length-1)
-		sendEmail(arrayPessoas[i], arrayPessoas[i+1]);
-	else
-		sendEmail(arrayPessoas[i], arrayPessoas[0]);
-};
+for (var i = 0; i < arrayPessoas.length; i++){
+	if (i != arrayPessoas.length-1){
+    sendEmail(arrayPessoas[i], arrayPessoas[i+1]);
+  } else {
+    sendEmail(arrayPessoas[i], arrayPessoas[0]);
+  }
+}
